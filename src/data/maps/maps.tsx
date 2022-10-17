13f952be0ff7a6831401e1e29ec8f7fb29a07c50
ArnoDeKoninck <arnodekoninck.contact@gmail.com>
2022-10-17 16:37:26 +0200

    Fixed lowercase barovia map image

--- src/data/maps/maps.tsx
+++ src/data/maps/maps.tsx
@@ -3,7 +3,7 @@ import { BaroviaLocations } from "../MapLocations/Barovia/BaroviaLocation";
 import { DeathHouseLocations } from "../MapLocations/DeathHouse/DeathHouseLocations";
 import { VillageOfBaroviaLocations } from "../MapLocations/VillageOfBarovia/VillageOfBarovia";
 export const AllMaps: Maps[] = [
-	{ name: "Barovia", img: "/images/Barovia_WorldMap.webp", locations: BaroviaLocations },
+	{ name: "Barovia", img: "/images/barovia_worldmap.webp", locations: BaroviaLocations },
 	{ name: "Death House", img: "/images/deathhouse_map.webp", locations: DeathHouseLocations },
 	{ name: "The Village of Barovia", img: "/images/village_of_barovia.webp", locations: VillageOfBaroviaLocations },
 ];
